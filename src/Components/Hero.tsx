e16fbcc8b824eafd2f3db51a6f9d539c886fa5a2
Olivier Lamothe <olamothe@coveo.com>
2021-07-28 10:45:46 -0400

    initial commit

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,9 +1,8 @@
-import {Grid, Typography} from '@material-ui/core';
+import {Typography} from '@material-ui/core';
 import React from 'react';
 import './Hero.css';
 
 interface IHeroProps {
-  logos: string[];
   welcome: string;
 }
 
@@ -14,48 +13,14 @@ function Anchor(props: React.HTMLProps<HTMLAnchorElement>) {
 const Hero: React.FunctionComponent<IHeroProps> = (props) => {
   return (
     <header className="App-header">
-      <Grid className="logo-container">
-        {props.logos.map((logo) => (
-          <img src={logo} className="App-logo" alt="logo" />
-        ))}
-      </Grid>
 
       <Typography variant="h5">{props.welcome}</Typography>
-      <Typography variant="body1">
-        This sample search page was built with{' '}
-        <Anchor
-          href="https://material-ui.com/getting-started/installation"
-          value="Material-ui"
-        />{' '}
-        and the{' '}
-        <Anchor
-          href="https://docs.coveo.com/en/headless"
-          value="Coveo Headless Library"
-        />
-        .<br></br>
-      </Typography>
       <Typography variant="body1">
         Edit{' '}
         <b>
           <code>src/App.tsx</code>
         </b>{' '}
         and save to reload.
-      </Typography>
-
-      <Typography variant="subtitle1" component="h6">
-        Customize your search page theme
-      </Typography>
-      <Typography variant="body1">
-        You can customize your theme by modifying the{' '}
-        <b>
-          <code>src/theme.tsx</code>
-        </b>
-        . For more information about theme customization, visit{' '}
-        <Anchor
-          href="https://material-ui.com/customization/theming/"
-          value="Material-ui theming"
-        />
-        .
       </Typography>
 
       <Typography variant="subtitle1" component="h6">
--- src/common/Engine.tsx
+++ src/common/Engine.tsx
@@ -1,31 +1,10 @@
-import {buildSearchEngine} from '@coveo/headless';
-
-const getEndpointToLocalServer = () => {
-  if (!process.env.REACT_APP_SERVER_PORT) {
-    throw new Error('Undefined "REACT_APP_SERVER_PORT" environment variable');
-  }
-  const port = process.env.REACT_APP_SERVER_PORT;
-  const pathname = '/token';
-  return `http://localhost:${port}${pathname}`;
-};
-
-const getTokenEndpoint = () => {
-  return process.env.REACT_APP_TOKEN_ENDPOINT || getEndpointToLocalServer();
-};
-
-export async function getSearchToken() {
-  const res = await fetch(getTokenEndpoint());
-  const {token} = await res.json();
-  return token;
-}
+import {
+  buildSearchEngine,
+  getSampleSearchEngineConfiguration,
+} from "@coveo/headless";
 
 export async function initializeHeadlessEngine() {
   return buildSearchEngine({
-    configuration: {
-      platformUrl: process.env.REACT_APP_PLATFORM_URL,
-      organizationId: process.env.REACT_APP_ORGANIZATION_ID!,
-      accessToken: await getSearchToken(),
-      renewAccessToken: getSearchToken,
-    },
+    configuration: getSampleSearchEngineConfiguration(),
   });
 }
